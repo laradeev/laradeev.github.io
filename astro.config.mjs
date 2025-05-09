af3fce6192f5612e8d8cbc6d1e00d8a4cb3c9394
Randy <randydev24@gmail.com>
2025-05-08 23:37:11 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
 
+export default defineConfig({
+  site: '.laradeev.github.io',
+})
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
